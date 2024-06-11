650b4cd8228a6822f81da0e3a1e8672232b7e38f
anaesbeidycervantes <161491649+anaesbeidycervantes@users.noreply.github.com>
2024-06-11 10:51:14 -0600

    Update index.js

--- BakcEnd/index.js
+++ BakcEnd/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const cors = require('cors');
+const path = require('path');
 const app = express();
 const port = 3002;
 
 let values = [];
 
+app.use(cors());
 app.use(express.json());
+
+// Ruta para servir archivos estáticos desde la carpeta FrontEnd
+app.use(express.static(path.join(__dirname, '../FrontEnd')));
+
+// Ruta para enviar index.html cuando se accede a la raíz
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../FrontEnd', 'index.html'));
+});
 
 app.get('/api/values', (req, res) => {
     res.json(values);
@@ -14,7 +25,7 @@ app.get('/api/data', (req, res) => {
     const { x, y, z } = req.query;
     const newValue = { x: parseFloat(x), y: parseFloat(y), z: parseFloat(z) };
     values.push(newValue);
-    if (values.length > 100) values.shift(); // Mantener solo los últimos 100 valores
+    if (values.length > 100) values.shift();
     res.json(newValue);
 });
 
